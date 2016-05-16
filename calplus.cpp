c2493c703f570d6ec2ddf6e5501adb77faa15822
Maeng-Min-Ju <mangssiee@icloud.com>
2016-05-16 20:01:55 +0900

    commit calplus

---
+++ calplus.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a,b;
+    char p;
+    
+    cin >> a >> b >> p;
+    
+    switch(p)
+    {
+        case '+':
+            cout << a+b << endl;
+            break;
+    }
+    return 0;
+}
